29822b4a128f241d70f9d134bc92e13218c2bd62
Nathan Trautenberg <Natester123@mac.com>
2023-12-15 13:02:46 -0700

    added css formatting; moved it to application.html.

--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -14,10 +14,10 @@ RSpec.describe "Search" do
     click_button "Search For Members"
 
     expect(page).to have_content("Total number of members: 97")
-    expect(page).to have_content("Name: Azula")
-    expect(page).to have_content("Affiliation: Azula's team (formerly) Dai Li (formerly) Fire Nation Fire Nation Royal Family Fire Warriors Royal Fire Academy for Girls (formerly)")
-    expect(page).to have_content("Allies: Ozai, Zuko")
-    expect(page).to have_content("Enemies: Iroh, Zuko, Kuei, Long Feng, Mai, Ty Lee, Ursa") 
+    expect(page).to have_content("Azula")
+    expect(page).to have_content("Azula's team (formerly) Dai Li (formerly) Fire Nation Fire Nation Royal Family Fire Warriors Royal Fire Academy for Girls (formerly)")
+    expect(page).to have_content("Ozai, Zuko")
+    expect(page).to have_content("Iroh, Zuko, Kuei, Long Feng, Mai, Ty Lee, Ursa") 
   end
 
 end
